69150cda41643947a59e382a212b29f29ef3156b
Pelle Wessman <pelle@kodfabrik.se>
2008-11-21 18:09:22 +0100

    Made a code review of the module and corrected most of the things. A few space related things as well as a few documentation things I didn't fix.

--- cobalt_admin.inc.php
+++ cobalt_admin.inc.php
@@ -12,7 +12,7 @@ function cobalt_settings() {
   $form['cobalt_theme'] = array(
     '#type' => 'select',
     '#options' => $theme_options,
-    '#default_value' => variable_get('cobalt_theme',''),
+    '#default_value' => variable_get('cobalt_theme', ''),
     '#title' => t('Select a theme for cobalt'),
   );
   
@@ -23,19 +23,19 @@ function cobalt_settings() {
   
   $form['javascript_includes']['cobalt_gears_init'] = array(
     '#type' => 'checkbox',
-    '#default_value' => variable_get('cobalt_gears_init',1),
+    '#default_value' => variable_get('cobalt_gears_init', 1),
     '#title' => t('Include the Google Gears init javascript'),
   );
   
   $form['javascript_includes']['cobalt_jquery_json'] = array(
     '#type' => 'checkbox',
-    '#default_value' => variable_get('cobalt_jquery_json',1),
+    '#default_value' => variable_get('cobalt_jquery_json', 1),
     '#title' => t('Include the jQuery JSON plugin'),
   );
   
   $form['javascript_includes']['cobalt_jquery_hotkeys'] = array(
     '#type' => 'checkbox',
-    '#default_value' => variable_get('cobalt_jquery_hotkeys',1),
+    '#default_value' => variable_get('cobalt_jquery_hotkeys', 1),
     '#title' => t('Include the jQuery Hotkeys plugin'),
   );
   
