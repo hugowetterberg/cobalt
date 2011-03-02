6b48d564f013226b015eb77bdf1528cb950a80f3
Anders Fajerson <anders@perifer.se>
2011-03-02 08:55:46 +0100

    Added anonymous wrapper function (as in 7.x-1.x) and removed trailing whitespace.

--- js/cobalt.menu.js
+++ js/cobalt.menu.js
@@ -1,92 +1,94 @@
-$(document).bind('cobalt-load', function(evt, cobalt) {  
-  var plugin = {
-    'version': 0,
-    'catalogs': {},
-    'handlers': []
-  };
-  
-  plugin['catalogs']['menu'] = {
-    'update': function(last_update, callback) {
-      $.getJSON(Drupal.settings.basePath + 'cobalt/data/menu_json', {}, function (data) {
-       cobalt.emptyCatalog('menu');
-        for (var id in data) {
-          cobalt.addEntry({id:id, name:data[id][1], extra:data[id][0], information: data[id][0], catalog:'menu', classname:'url_data'});
+(function($) {
+  $(document).bind('cobalt-load', function(evt, cobalt) {
+    var plugin = {
+      'version': 0,
+      'catalogs': {},
+      'handlers': []
+    };
+
+    plugin['catalogs']['menu'] = {
+      'update': function(last_update, callback) {
+        $.getJSON(Drupal.settings.basePath + 'cobalt/data/menu_json', {}, function (data) {
+         cobalt.emptyCatalog('menu');
+          for (var id in data) {
+            cobalt.addEntry({id:id, name:data[id][1], extra:data[id][0], information: data[id][0], catalog:'menu', classname:'url_data'});
+          }
+          callback(false);
+        });
+      },
+      'install': function() {
+      },
+      'uninstall': function() {
+      },
+      'item_formatter': function(item) {
+        return item.name + ' <small>' + item.information + '</small>';
+      },
+      'update_rate': 60000
+    };
+
+    var uri_from_item = function(item, omitDestination) {
+      var path = item.information;
+      var destination = Drupal.settings.cobalt.path;
+      if (typeof(path) == 'object') {
+        destination = path.destination;
+        path = path.path;
+      }
+
+      if (path=='<front>') {
+        path = '';
+      }
+
+      if (destination && !omitDestination) {
+        path = path + '?destination=' + destination;
+      }
+
+      return Drupal.settings.basePath + path;
+    };
+
+    plugin['handlers'].push({
+      'id': 'menu_goto',
+      'name': Drupal.t('Go to and return'),
+      'data_class': 'url_data',
+      'handler': function(text, item) {
+        window.location.href = uri_from_item(item);
+      }
+    });
+
+    plugin['handlers'].push({
+      'id': 'menu_goto_stay',
+      'name': Drupal.t('Go to'),
+      'data_class': 'url_data',
+      'handler': function(text, item) {
+        window.location.href = uri_from_item(item, true);
+      }
+    });
+
+    plugin['handlers'].push({
+      'id': 'menu_open_in_new_window',
+      'name': Drupal.t('Open in new window'),
+      'data_class': 'url_data',
+      'handler': function(text, item) {
+        var form = document.createElement("form");
+        $(form).attr({
+          'method': 'GET',
+          'action': uri_from_item(item),
+          'target': '_blank'
+        }).appendTo('body');
+
+        try {
+          form.submit();
         }
-        callback(false);
-      });
-    },
-    'install': function() {
-    },
-    'uninstall': function() {
-    },
-    'item_formatter': function(item) {
-      return item.name + ' <small>' + item.information + '</small>';
-    },
-    'update_rate': 60000
-  };
-  
-  var uri_from_item = function(item, omitDestination) {
-    var path = item.information;
-    var destination = Drupal.settings.cobalt.path;
-    if (typeof(path) == 'object') {
-      destination = path.destination;
-      path = path.path;
-    }
+        catch(e) {
+          var message = $('<div></div>');
+          message.append('<h1>' + Drupal.t('Could not open window') + '</h1>');
+          message.append('<p>' + Drupal.t('You might be using a popup blocker, which stopped Cobalt from opening a new window.') + '</p>');
+          cobalt.showHtml(message);
+        }
 
-    if (path=='<front>') {
-      path = '';
-    }
-
-    if (destination && !omitDestination) {
-      path = path + '?destination=' + destination;
-    }
-
-    return Drupal.settings.basePath + path;
-  };
-
-  plugin['handlers'].push({
-    'id': 'menu_goto',
-    'name': Drupal.t('Go to and return'),
-    'data_class': 'url_data',
-    'handler': function(text, item) {
-      window.location.href = uri_from_item(item);
-    }
-  });
-
-  plugin['handlers'].push({
-    'id': 'menu_goto_stay',
-    'name': Drupal.t('Go to'),
-    'data_class': 'url_data',
-    'handler': function(text, item) {
-      window.location.href = uri_from_item(item, true);
-    }
-  });
-
-  plugin['handlers'].push({
-    'id': 'menu_open_in_new_window',
-    'name': Drupal.t('Open in new window'),
-    'data_class': 'url_data',
-    'handler': function(text, item) {
-      var form = document.createElement("form");
-      $(form).attr({
-        'method': 'GET',
-        'action': uri_from_item(item),
-        'target': '_blank'
-      }).appendTo('body');
-      
-      try {
-        form.submit();
+        $(form).remove();
       }
-      catch(e) {
-        var message = $('<div></div>');
-        message.append('<h1>' + Drupal.t('Could not open window') + '</h1>');
-        message.append('<p>' + Drupal.t('You might be using a popup blocker, which stopped Cobalt from opening a new window.') + '</p>');
-        cobalt.showHtml(message);
-      }
-      
-      $(form).remove();
-    }
+    });
+
+    cobalt.registerPlugin('cobalt_menu', plugin);
   });
-  
-  cobalt.registerPlugin('cobalt_menu', plugin);
-});
+})(jQuery);
--- js/cobalt.users.js
+++ js/cobalt.users.js
@@ -1,63 +1,65 @@
-$(document).bind('cobalt-load', function(evt, cobalt) {
-  var plugin = {
-    'version': 0,
-    'catalogs': {},
-    'handlers': []
-  };
-  
-  plugin['catalogs']['users'] = {
-    'update': function(last_update, callback) {
-      $.getJSON(Drupal.settings.basePath + 'cobalt/data/users_json/' + Math.round((last_update/1000)), {}, function (data) {
-        var num_nodes = data.length;
-        for (var i=0; i<num_nodes; i++) {
-          cobalt.addEntry({id:data[i][0], name:data[i][1], information:{'perm': data[i][2]}, catalog:'users', classname:'user'});
-        }
-        callback(true);
-      });
-    },
-    'install': function() {
-    },
-    'uninstall': function() {
-    },
-    'update_rate': 300000
-  };
-  
-  // Add handlers
-  plugin['handlers'].push({
-    'id': 'user_view',
-    'name': Drupal.t('View'),
-    'data_class': 'user',
-    'applicable': function(text, item) {
-      return item.information.perm.indexOf('r') >= 0;
-    },
-    'handler': function(text, item) {
-      window.location.href = Drupal.settings.basePath + 'user/' + item.id;
-    }
+(function($) {
+  $(document).bind('cobalt-load', function(evt, cobalt) {
+    var plugin = {
+      'version': 0,
+      'catalogs': {},
+      'handlers': []
+    };
+
+    plugin['catalogs']['users'] = {
+      'update': function(last_update, callback) {
+        $.getJSON(Drupal.settings.basePath + 'cobalt/data/users_json/' + Math.round((last_update/1000)), {}, function (data) {
+          var num_nodes = data.length;
+          for (var i=0; i<num_nodes; i++) {
+            cobalt.addEntry({id:data[i][0], name:data[i][1], information:{'perm': data[i][2]}, catalog:'users', classname:'user'});
+          }
+          callback(true);
+        });
+      },
+      'install': function() {
+      },
+      'uninstall': function() {
+      },
+      'update_rate': 300000
+    };
+
+    // Add handlers
+    plugin['handlers'].push({
+      'id': 'user_view',
+      'name': Drupal.t('View'),
+      'data_class': 'user',
+      'applicable': function(text, item) {
+        return item.information.perm.indexOf('r') >= 0;
+      },
+      'handler': function(text, item) {
+        window.location.href = Drupal.settings.basePath + 'user/' + item.id;
+      }
+    });
+
+    plugin['handlers'].push({
+      'id': 'user_edit',
+      'name': Drupal.t('Edit'),
+      'data_class': 'user',
+      'applicable': function(text, item) {
+        return item.information.perm.indexOf('w') >= 0;
+      },
+      'handler': function(text, item) {
+        window.location.href = Drupal.settings.basePath + 'user/' + item.id + '/edit?destination=' + Drupal.settings.cobalt.path;
+      }
+    });
+
+    plugin['handlers'].push({
+      'id': 'user_delete',
+      'name': Drupal.t('Delete'),
+      'data_class': 'user',
+      'applicable': function(text, item) {
+        return item.information.perm.indexOf('d') >= 0;
+      },
+      'handler': function(text, item) {
+        window.location.href = Drupal.settings.basePath + 'user/' + item.id + '/delete?destination=' + Drupal.settings.cobalt.path;
+      }
+    });
+
+    cobalt.registerPlugin('cobaltusers', plugin);
   });
-  
-  plugin['handlers'].push({
-    'id': 'user_edit',
-    'name': Drupal.t('Edit'),
-    'data_class': 'user',
-    'applicable': function(text, item) {
-      return item.information.perm.indexOf('w') >= 0;
-    },
-    'handler': function(text, item) {
-      window.location.href = Drupal.settings.basePath + 'user/' + item.id + '/edit?destination=' + Drupal.settings.cobalt.path;
-    }
-  });
-  
-  plugin['handlers'].push({
-    'id': 'user_delete',
-    'name': Drupal.t('Delete'),
-    'data_class': 'user',
-    'applicable': function(text, item) {
-      return item.information.perm.indexOf('d') >= 0;
-    },
-    'handler': function(text, item) {
-      window.location.href = Drupal.settings.basePath + 'user/' + item.id + '/delete?destination=' + Drupal.settings.cobalt.path;
-    }
-  });
-  
-  cobalt.registerPlugin('cobaltusers', plugin);
-});
+})(jQuery);
